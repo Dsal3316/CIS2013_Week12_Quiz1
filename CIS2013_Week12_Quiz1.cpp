c7e4a2e06dfe6ce4d0a1e78f15d5bfaf8fa2cf75
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-04-11 19:12:41 -0500

    commit

--- CIS2013_Week12_Quiz1.cpp
+++ CIS2013_Week12_Quiz1.cpp
@@ -52,7 +52,7 @@ class house
 		value += (car_ports * 1000);
 		value += (windows * 200);
 		value += (doors * 1000);
-		cout<<" value of house is " << value << "dollars"<< endl;
+		cout<<" value of house is " << value<< " " << "dollars" <<endl;
 		}
 
 		
